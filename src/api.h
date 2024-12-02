0876b3f6929c11e362cc025fc77c99738c8704a2
csep7472 <101598208+csep7472@users.noreply.github.com>
2024-12-01 21:12:47 -0500

    Create api.h to separate the api.cpp into an h/cpp to be incorporated into the main

---
+++ src/api.h
@@ -0,0 +1,28 @@
+#ifndef API_H
+#define API_H
+
+#include <string>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+// Game class definition
+class Game {
+public:
+    double rating = 0.0;              
+    std::string name;                 
+    std::string genres;               
+    std::string platforms;            
+    std::string perspectives;         
+    std::string gameModes;            
+    int releaseYear = 0;              
+
+    // Nlohmann JSON serialization support
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(Game, rating, name, genres, platforms, perspectives, gameModes, releaseYear)
+};
+
+// Function declarations
+size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata);
+std::string joinArray(const nlohmann::json& array, const std::string& key);
+void fetchGames(std::vector<Game>& allGames);
+
+#endif
