49ad14865aa378bc262a02674df5c78b58a1d497
ShwinLi <liandrew0404@gmail.com>
2024-12-04 00:46:37 -0500

    guess now print as results in rows

--- src/GUI.h
+++ src/GUI.h
@@ -8,14 +8,17 @@ using namespace std;
 class TextDisplay {
 private:
     sf::Text text;
-    sf::Font font;
 
 public:
-    TextDisplay(const string& string, int size, const sf::Vector2f& position, sf::Color color) {
-        if (!font.loadFromFile("../resources/MontserratRegular-BWBEl.ttf")) {
-            throw std::runtime_error("Failed to load font");
-        }
+    TextDisplay() {
 
+        text.setString("");
+        text.setCharacterSize(30);
+        text.setFillColor(sf::Color::Black);
+        text.setPosition(sf::Vector2f(0, 0));
+    }
+
+    TextDisplay(sf::Font& font, const string& string, int size, const sf::Vector2f& position, sf::Color color) {
         text.setFont(font);
         text.setString(string);
         text.setCharacterSize(size);
@@ -37,31 +40,6 @@ public:
         window.draw(text);
     }
 };
-
-
-class GraphicDisplay {
-private:
-    sf::Sprite sprite;
-    sf::Texture texture;
-
-public:
-    GraphicDisplay(const std::string& textureFilePath, const sf::Vector2f& position) {
-        if (!texture.loadFromFile(textureFilePath)) {
-            throw std::runtime_error("Failed to load texture");
-        }
-
-        sprite.setTexture(texture);
-        sprite.setPosition(position);
-    }
-
-    void setPosition(const sf::Vector2f& newPosition) {
-        sprite.setPosition(newPosition);
-    }
-    void draw(sf::RenderWindow& window) const {
-        window.draw(sprite);
-    }
-};
-
 
 
 #endif //GUI_H
--- src/main.cpp
+++ src/main.cpp
@@ -102,6 +102,8 @@ vector<string> evaluateGuess(HashMap<string, Game>& gameMap, string& userGuess, 
     return results;
 }
 
+
+
 int main() {
     vector<Game> allGames = getGamesList();
     SplayTree splaytree;
@@ -110,20 +112,23 @@ int main() {
 
     // gets window size to dynamically adjust text and button locations
     sf::Vector2u windowSize = window.getSize();
-
+    sf::Font font;
+    if (!font.loadFromFile("../resources/MontserratRegular-BWBEl.ttf")) {
+        throw std::runtime_error("Failed to load font");
+    }
     // creating and setting position for title text
-    TextDisplay title("Video Game Wordle", 50,  sf::Vector2f(0, 0), sf::Color::Black);
+    TextDisplay title(font, "Video Game Wordle", 50,  sf::Vector2f(0, 0), sf::Color::Black);
     sf::FloatRect titleRect = title.getText().getGlobalBounds();
     float centerX = (windowSize.x - titleRect.width) / 2;
     title.setPosition(sf::Vector2f(centerX-10, windowSize.y * 0.1f));
 
     // creating and setting position for Splay Tree and Hash Map text
-    TextDisplay splayText("Splay Tree", 30, sf::Vector2f(centerX, windowSize.y * 0.5f), sf::Color::Black);
-    TextDisplay hashText("Hash Map", 30, sf::Vector2f(centerX + titleRect.width, windowSize.y * 0.5f), sf::Color::Black);
+    TextDisplay splayText(font, "Splay Tree", 30, sf::Vector2f(centerX, windowSize.y * 0.5f), sf::Color::Black);
+    TextDisplay hashText(font, "Hash Map", 30, sf::Vector2f(centerX + titleRect.width, windowSize.y * 0.5f), sf::Color::Black);
     sf::FloatRect hashRect = hashText.getText().getGlobalBounds();
     hashText.setPosition(sf::Vector2f(centerX + titleRect.width - hashRect.width, windowSize.y * 0.5f));
 
-    TextDisplay welcomeText("Select a Game Mode: ", 40, sf::Vector2f(centerX, windowSize.y * 0.3f), sf::Color::Black);
+    TextDisplay welcomeText(font, "Select a Game Mode: ", 40, sf::Vector2f(centerX, windowSize.y * 0.3f), sf::Color::Black);
     sf::FloatRect welcomeTextRect = welcomeText.getText().getGlobalBounds();
     float welcomeCenter = (windowSize.x - welcomeTextRect.width) / 2;
     welcomeText.setPosition(sf::Vector2f(welcomeCenter, windowSize.y * 0.3));
@@ -137,20 +142,20 @@ int main() {
     hashBox.setFillColor(sf::Color::Blue);
 
     // create and position time it takes to insert
-    TextDisplay insertTime("time to insert all games: ", 20, sf::Vector2f(0, windowSize.y - 30), sf::Color::Black);
+    TextDisplay insertTime(font, "time to insert all games: ", 20, sf::Vector2f(0, windowSize.y - 30), sf::Color::Black);
 
     // user input area
     sf::RectangleShape searchBox(sf::Vector2f(600, 30));
     searchBox.setPosition((windowSize.x - searchBox.getSize().x) / 2, windowSize.y * 0.2);
     searchBox.setOutlineThickness(3);
     searchBox.setOutlineColor(sf::Color::Black);
-    TextDisplay input("Enter Your Guess here", 20, sf::Vector2f(0, 0), sf::Color::Black);
+    TextDisplay input(font, "Enter Your Guess here", 20, sf::Vector2f(0, 0), sf::Color::Black);
     input.setPosition(sf::Vector2f(searchBox.getPosition().x + 5, windowSize.y * 0.2));
     sf::RectangleShape guessButton(sf::Vector2f(80, 30));
     guessButton.setPosition(searchBox.getPosition().x + 620, windowSize.y * 0.2);
     guessButton.setOutlineThickness(3);
     guessButton.setOutlineColor(sf::Color::Black);
-    TextDisplay guessText("Guess!", 20, sf::Vector2f(0, 0), sf::Color::Black);
+    TextDisplay guessText(font, "Guess!", 20, sf::Vector2f(0, 0), sf::Color::Black);
     sf::FloatRect guessTextRect = guessText.getText().getGlobalBounds();
     guessText.setPosition(sf::Vector2f(guessButton.getPosition().x + (guessButton.getSize().x - guessTextRect.width) / 2, windowSize.y * 0.2));
 
@@ -163,11 +168,11 @@ int main() {
         boxes[i].setFillColor(sf::Color::White);
         boxes[i].setPosition((windowSize.x - (5 * 150)) / 6.0f * (i + 1) + 150 * i, windowSize.y * 0.05);
     }
-    TextDisplay red("Red: None of the \nclues match.", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (0 + 1) + 150 * 0 + 5, windowSize.y * 0.05), sf::Color::Black);
-    TextDisplay yellow("Yellow: One or \nmore of the clues \nmatch, but not all.", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (1 + 1) + 150 * 1 + 5, windowSize.y * 0.05), sf::Color::Black);
-    TextDisplay green("Green: All clues \nmatch!", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (2 + 1) + 150 * 2 + 5, windowSize.y * 0.05), sf::Color::Black);
-    TextDisplay up("Arrow Up: The \ngame you have \nguessed is older \nthan the correct \ngame", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (3 + 1) + 150 * 3 + 5, windowSize.y * 0.05), sf::Color::Black);
-    TextDisplay down("Arrow down: The \ngame you have \nguessed is newer \nthan the correct \ngame", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (4 + 1) + 150 * 4 + 5, windowSize.y * 0.05), sf::Color::Black);
+    TextDisplay red(font, "Red: None of the \nclues match.", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (0 + 1) + 150 * 0 + 5, windowSize.y * 0.05), sf::Color::Black);
+    TextDisplay yellow(font, "Yellow: One or \nmore of the clues \nmatch, but not all.", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (1 + 1) + 150 * 1 + 5, windowSize.y * 0.05), sf::Color::Black);
+    TextDisplay green(font, "Green: All clues \nmatch!", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (2 + 1) + 150 * 2 + 5, windowSize.y * 0.05), sf::Color::Black);
+    TextDisplay up(font, "Arrow Up: The \ngame you have \nguessed is older \nthan the correct \ngame", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (3 + 1) + 150 * 3 + 5, windowSize.y * 0.05), sf::Color::Black);
+    TextDisplay down(font, "Arrow down: The \ngame you have \nguessed is newer \nthan the correct \ngame", 15,sf::Vector2f((windowSize.x - (5 * 150)) / 6.0f * (4 + 1) + 150 * 4 + 5, windowSize.y * 0.05), sf::Color::Black);
 
     // boxes for guess hints
     vector<sf::RectangleShape> hints(7);
@@ -178,23 +183,36 @@ int main() {
         hints[i].setFillColor(sf::Color::White);
         hints[i].setPosition((windowSize.x - (7 * 80)) / 8.0f * (i + 1) + 80 * i, windowSize.y * 0.25);
     }
-    vector<TextDisplay> hint_displays;
-    TextDisplay name("Name", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (0 + 1) + 80 * 0 + 5, windowSize.y * 0.26), sf::Color::Black);
-    TextDisplay genre("Genres", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (1 + 1) + 80 * 1 + 5, windowSize.y * 0.26), sf::Color::Black);
-    TextDisplay platform("Platform", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (2 + 1) + 80 * 2 + 5, windowSize.y * 0.26), sf::Color::Black);
-    TextDisplay pers("Perspectives", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (3 + 1) + 80 * 3 + 5, windowSize.y * 0.26), sf::Color::Black);
-    TextDisplay gamemode("Game Modes", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (4 + 1) + 80 * 4 + 5, windowSize.y * 0.26), sf::Color::Black);
-    TextDisplay year("Release Year", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (5 + 1) + 80 * 5 + 5, windowSize.y * 0.26), sf::Color::Black);
-    TextDisplay rating("Ratings", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (6 + 1) + 80 * 6 + 5, windowSize.y * 0.26), sf::Color::Black);
+    TextDisplay name(font, "Name", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (0 + 1) + 80 * 0 + 5, windowSize.y * 0.26), sf::Color::Black);
+    TextDisplay genre(font, "Genres", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (1 + 1) + 80 * 1 + 5, windowSize.y * 0.26), sf::Color::Black);
+    TextDisplay platform(font, "Platform", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (2 + 1) + 80 * 2 + 5, windowSize.y * 0.26), sf::Color::Black);
+    TextDisplay pers(font, "Perspectives", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (3 + 1) + 80 * 3 + 5, windowSize.y * 0.26), sf::Color::Black);
+    TextDisplay gamemode(font, "Game Modes", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (4 + 1) + 80 * 4 + 5, windowSize.y * 0.26), sf::Color::Black);
+    TextDisplay year(font, "Release Year", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (5 + 1) + 80 * 5 + 5, windowSize.y * 0.26), sf::Color::Black);
+    TextDisplay rating(font, "Ratings", 12,sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (6 + 1) + 80 * 6 + 5, windowSize.y * 0.26), sf::Color::Black);
 
     // game not found
-    TextDisplay notFound("Game Not Found", 12, sf::Vector2f(0, 0), sf::Color::Red);
+    TextDisplay notFound(font, "Game Not Found", 12, sf::Vector2f(windowSize.x, windowSize.y), sf::Color::Red);
 
     // game stuff
     bool gameStarted = false;
     string inputText = "";
     string mode = "";
-    vector<string> guesses;
+    Game* guessedGame;
+    vector<string> names;
+    vector<string> genres;
+    vector<string> platforms;
+    vector<string> perspectives;
+    vector<string> gameModes;
+    vector<int> years;
+    vector<double> ratings;
+    vector<TextDisplay> nameDisplays;
+    vector<TextDisplay> genreDisplays;
+    vector<TextDisplay> platformDisplays;
+    vector<TextDisplay> perspectiveDisplays;
+    vector<TextDisplay> modeDisplays;
+    vector<TextDisplay> yearDisplays;
+    vector<TextDisplay> ratingDisplays;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -244,15 +262,50 @@ int main() {
 
                 else if (event.text.unicode == '\r' && inputText.size() > 0) {
                     if (mode == "splay") {
-                        if (splaytree.search(inputText) == nullptr) {
+                        Game* guessedGame = splaytree.search(inputText);
+                        // guessed game is not in database
+                        if (guessedGame == nullptr) {
                             notFound.setText("Game Not Found");
                             notFound.setPosition(sf::Vector2f((windowSize.x - searchBox.getSize().x) / 2, windowSize.y * 0.2 + 30));
                         }
                         else {
-                            if (find(guesses.begin(), guesses.end(), inputText) == guesses.end()) {
-                                guesses.push_back(inputText);
-                                notFound.setPosition(sf::Vector2f(0, 0));
+                            // gussed game found in database
+                            if (find(names.begin(), names.end(), inputText) == names.end()) {
+                                // after hitting enter, everything in the search box disappears
+                                inputText = "";
+                                // remove search error from screen
+                                notFound.setPosition(sf::Vector2f(windowSize.x, windowSize.y));
+                                // append all the hint info into vectors
+                                names.push_back(guessedGame->name);
+                                genres.push_back(guessedGame->genres);
+                                platforms.push_back(guessedGame->platforms);
+                                perspectives.push_back(guessedGame->perspectives);
+                                gameModes.push_back(guessedGame->gameModes);
+                                years.push_back(guessedGame->releaseYear);
+                                ratings.push_back(guessedGame->rating);
+                                // replace the comma with \n so they print on different lines
+                                for (int i = 0; i < names.size(); i++) {
+                                    std::replace(names[i].begin(), names[i].end(), ',', '\n');
+                                    std::replace(genres[i].begin(), genres[i].end(), ',', '\n');
+                                    std::replace(platforms[i].begin(), platforms[i].end(), ',', '\n');
+                                    std::replace(perspectives[i].begin(), perspectives[i].end(), ',', '\n');
+                                    std::replace(gameModes[i].begin(), gameModes[i].end(), ',', '\n');
+                                }
+                                // append to vector so that they can be displayed
+                                for (size_t i = 0; i < names.size(); i++) {
+                                    float firstRowY = windowSize.y * 0.3f;
+                                    float verticalSpace = windowSize.y * 0.9f - firstRowY;
+                                    float spaceBetweenRows = verticalSpace / 6.0f;
+                                    nameDisplays.push_back(TextDisplay(font, names[i], 12, sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (0 + 1) + 80 * 0 + 5, firstRowY + (i * spaceBetweenRows)), sf::Color::Black));
+                                    genreDisplays.push_back(TextDisplay(font, genres[i], 12, sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (1 + 1) + 80 * 1 + 5, firstRowY + (i * spaceBetweenRows)), sf::Color::Black));
+                                    platformDisplays.push_back(TextDisplay(font, platforms[i], 12, sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (2 + 1) + 80 * 2 + 5, firstRowY + (i * spaceBetweenRows)), sf::Color::Black));
+                                    perspectiveDisplays.push_back(TextDisplay(font, perspectives[i], 12, sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (3 + 1) + 80 * 3 + 5, firstRowY + (i * spaceBetweenRows)), sf::Color::Black));
+                                    modeDisplays.push_back(TextDisplay(font, gameModes[i], 12, sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (4 + 1) + 80 * 4 + 5, firstRowY + (i * spaceBetweenRows)), sf::Color::Black));
+                                    yearDisplays.push_back(TextDisplay(font, to_string(years[i]), 12, sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (5 + 1) + 80 * 5 + 5, firstRowY + (i * spaceBetweenRows)), sf::Color::Black));
+                                    ratingDisplays.push_back(TextDisplay(font, to_string(ratings[i]), 12, sf::Vector2f((windowSize.x - (7 * 80)) / 8.0f * (6 + 1) + 80 * 6 + 5, firstRowY + (i * spaceBetweenRows)), sf::Color::Black));
+                                }
                             }
+                            // guessed game found in database but already guessed
                             else {
                                 notFound.setText("Already Guessed");
                                 notFound.setPosition(sf::Vector2f((windowSize.x - searchBox.getSize().x) / 2, windowSize.y * 0.2 + 30));
@@ -286,9 +339,6 @@ int main() {
             for (int i = 0; i < 7; i++) {
                 window.draw(hints[i]);
             }
-            for (auto& hintDisplay : hint_displays) {
-                hintDisplay.draw(window);
-            }
             name.draw(window);
             genre.draw(window);
             platform.draw(window);
@@ -304,6 +354,15 @@ int main() {
             input.draw(window);
             guessText.draw(window);
             notFound.draw(window);
+            for (size_t i = 0; i < nameDisplays.size(); i++) {
+                nameDisplays[i].draw(window);
+                genreDisplays[i].draw(window);
+                platformDisplays[i].draw(window);
+                perspectiveDisplays[i].draw(window);
+                modeDisplays[i].draw(window);
+                yearDisplays[i].draw(window);
+                ratingDisplays[i].draw(window);
+            }
             window.display();
         }
         else {
