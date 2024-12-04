d9f581196262f1bad8307fc0f5598694cc004dd5
csep7472 <101598208+csep7472@users.noreply.github.com>
2024-12-04 08:27:23 -0500

    Update api.h

--- src/api.h
+++ src/api.h
@@ -8,16 +8,16 @@
 // Game class definition
 class Game {
 public:
-    double rating = 0.0;
-    std::string name;                 
-    std::string genres;               
-    std::string platforms;            
-    std::string perspectives;         
-    std::string gameModes;            
+    double rating = 0.0;              
+    std::string name;
+    std::vector<std::string> genres;
+    std::vector<std::string> platforms;
+    std::vector<std::string> perspectives;
+    std::vector<std::string> gameModes;
     int releaseYear = 0;              
 
     // Nlohmann JSON serialization support
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(Game, rating, name, genres, platforms, perspectives, gameModes, releaseYear);
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(Game, rating, name, genres, platforms, perspectives, gameModes, releaseYear)
 };
 
 // Function declarations
